9926b376c2646e95dfe12b8962d3d124ad1436a0
정성빈 <tjdqls3124@gmail.com>
2025-03-05 14:39:55 +0900

    아 진짜 모르겠다 하나도

--- minipr/src/jdbc/Test3.java
+++ minipr/src/jdbc/Test3.java
@@ -1,11 +1,12 @@
 package jdbc;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 // Statement 사용
 // CRUD 를 메소드화 - url, user, pwd 를 static
 //   메소드내에서 Connection, Statement, ResutSet 객체화
@@ -17,15 +18,19 @@ public class Test3 {
     // MySQL 에 접근하기 위해 필요한 3가지
     static String url = "jdbc:mysql://localhost:3306/madang";
     static String user = "root";
-    static String pwd = "1234";
+    static String pwd = "root";
 
     public static void main(String[] args) {
 
 //      insertCustomer(6, "손흥민", "영국 토트넘", "010-6666-6666");
-//        updateCustomer(6, "대한민국 서울");
-        deleteCustomer(6);
-//      listCustomer();
-//      detailCustomer();
+//      updateCustomer(6, "대한민국 서울");
+//      deleteCustomer(6);
+//      List<CustomerDto> list = listCustomer();  // 없으면 list 는 null 은 아니고 empty
+//      for (CustomerDto dto : list) {
+//          System.out.println(dto);
+//      }
+        CustomerDto dto = detailCustomer(2); // 없으면 null
+        System.out.println(dto);
     }
     static void insertCustomer(int custId, String name, String address, String phone) {
         Connection con = null;
@@ -90,10 +95,10 @@ public class Test3 {
 
         try {
             con = DriverManager.getConnection(url, user, pwd);
-            pstmt = con.createStatement(deleteSql);
+            pstmt = con.prepareStatement(deleteSql);
             pstmt.setInt(1, custId);
 
-            int ret = pstmt.executeUpdate(deleteSql);
+            int ret = pstmt.executeUpdate();
             System.out.println(ret);
 
         }catch(SQLException e) {
@@ -108,10 +113,12 @@ public class Test3 {
         }
     }
 
-    static void listCustomer() {
+    static List<CustomerDto> listCustomer() {
         Connection con = null;
         Statement stmt = null;
         ResultSet rs = null;
+
+        List<CustomerDto> list = new ArrayList<>();
 
         try {
             con = DriverManager.getConnection(url, user, pwd);
@@ -120,7 +127,14 @@ public class Test3 {
             String selectSql = "select custid, name, address, phone from customer; ";
             rs = stmt.executeQuery(selectSql);
             while(rs.next()) {
-                System.out.println(rs.getInt("custid") + " | " + rs.getString("name")+ " | " + rs.getString("address")+ " | " + rs.getString("phone"));
+                // 각 row 를 CustomerDto 객체로 만들고 ArrayList 담는다.
+                CustomerDto dto = new CustomerDto();
+                dto.setCustId(rs.getInt("custid"));
+                dto.setName(rs.getString("name"));
+                dto.setAddress(rs.getString("address"));
+                dto.setPhone(rs.getString("phone"));
+
+                list.add(dto);
             }
 
         }catch(SQLException e) {
@@ -134,33 +148,45 @@ public class Test3 {
                 e.printStackTrace();
             }
         }
+
+        return list;
     }
 
-    static void detailCustomer() {
+    static CustomerDto detailCustomer(int custId) {
         Connection con = null;
-        Statement stmt = null;
+        PreparedStatement pstmt = null;
         ResultSet rs = null;
+
+        CustomerDto dto = null;
+
+        String selectSql = "select custid, name, address, phone from customer where custid = ?; ";
 
         try {
             con = DriverManager.getConnection(url, user, pwd);
-            stmt = con.createStatement();
+            pstmt = con.prepareStatement(selectSql);
+            pstmt.setInt(1, custId);
 
-            String selectSql = "select custid, name, address, phone from customer where custid = 4; ";
-            rs = stmt.executeQuery(selectSql);
+            rs = pstmt.executeQuery();
             if(rs.next()) {
-                System.out.println(rs.getInt("custid") + " | " + rs.getString("name")+ " | " + rs.getString("address")+ " | " + rs.getString("phone"));
+                // 각 row 를 CustomerDto 객체로 만들고 ArrayList 담는다.
+                dto = new CustomerDto();
+                dto.setCustId(rs.getInt("custid"));
+                dto.setName(rs.getString("name"));
+                dto.setAddress(rs.getString("address"));
+                dto.setPhone(rs.getString("phone"));
             }
-
         }catch(SQLException e) {
             e.printStackTrace();
         }finally {
             try {
                 rs.close(); // row 가 있는 상태를 전제
-                stmt.close();
+                pstmt.close();
                 con.close();
             }catch(SQLException e) {
                 e.printStackTrace();
             }
         }
+
+        return dto;
     }
 }
